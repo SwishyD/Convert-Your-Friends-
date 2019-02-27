20f420cf1c78092e47ffb0c0a75a6d2e6aa26286
Cody <xiswishd@gmail.com>
2019-02-28 12:19:13 +1300

    Main Menu, Pause Menu, Goal Mechanic Setup

---
+++ Convert Your Friends!! Unity Project/Assets/_Scripts/Cody/GoalTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "NPC")
+        {
+            GameManager.instance.P1Goal();
+        }
+    }
+}
