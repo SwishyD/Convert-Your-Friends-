005cb57506a2550a204f1bb8e1b5af9f9ccebeb9
Cody <xiswishd@gmail.com>
2019-03-01 15:31:17 +1300

    Particles

---
+++ Convert Your Friends!! Unity Project/Assets/_Scripts/Cody/CharacterMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public float speed;
+    public float lift;
+
+    // Use this for initialization
+    void Start () {
+    
+
+}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.transform.position += new Vector3(-speed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.position += new Vector3(speed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position += new Vector3(0, 0, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position += new Vector3(0, 0, speed);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            gameObject.transform.position += new Vector3(0, speed, 0);
+        }
+
+    }
+}
