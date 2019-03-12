3e71a66c3624e165e2b119d7d56069aee5cdf5d7
Cody <xiswishd@gmail.com>
2019-03-12 14:58:04 +1300

    Respawning Mechanics, particle system functionality

--- Convert Your Friends!! Unity Project/Assets/_Scripts/Cody/GoalTrigger.cs
+++ Convert Your Friends!! Unity Project/Assets/_Scripts/Cody/GoalTrigger.cs
@@ -4,11 +4,67 @@ using UnityEngine;
 
 public class GoalTrigger : MonoBehaviour {
 
+    public bool player1Goal;
+
+    public GameObject[] p1Particles;
+    public GameObject[] p2Particles;
+    public Transform npcResPoint;
+
     private void OnTriggerEnter(Collider col)
-    {
-        if(col.gameObject.tag == "NPC")
+    { 
+        //When an NPC gets pushed into a pit//
+        if(col.gameObject.tag == "NPC" && player1Goal)
         {
+
+            for (int i = 0; i < p1Particles.Length; i++)
+            {
+                p1Particles[i].GetComponent<ParticleSystem>().Play();
+            }
             GameManager.instance.P1Goal();
+            col.transform.position = npcResPoint.position;
+        }
+        else if (col.gameObject.tag == "NPC" && !player1Goal)
+        {
+            for (int i = 0; i < p2Particles.Length; i++)
+            {
+                p2Particles[i].GetComponent<ParticleSystem>().Play();
+            }
+            GameManager.instance.P2Goal();
+            col.transform.position = npcResPoint.position;
+        }
+
+        //If players fall into pits//
+        if(col.gameObject.tag == "Player1" && !player1Goal)
+        {
+            for (int i = 0; i < p2Particles.Length; i++)
+            {
+                p2Particles[i].GetComponent<ParticleSystem>().Play();
+            }
+            GameManager.instance.Player2Sacrifice();
+        }
+        else if (col.gameObject.tag == "Player1" && player1Goal)
+        {
+            for (int i = 0; i < p1Particles.Length; i++)
+            {
+                p1Particles[i].GetComponent<ParticleSystem>().Play();
+            }
+            GameManager.instance.RespawnP1();
+        }
+        if (col.gameObject.tag == "Player2" && player1Goal)
+        {
+            for (int i = 0; i < p1Particles.Length; i++)
+            {
+                p1Particles[i].GetComponent<ParticleSystem>().Play();
+            }
+            GameManager.instance.Player1Sacrifice();
+        }
+        else if (col.gameObject.tag == "Player2" && !player1Goal)
+        {
+            for (int i = 0; i < p2Particles.Length; i++)
+            {
+                p2Particles[i].GetComponent<ParticleSystem>().Play();
+            }
+            GameManager.instance.RespawnP2();
         }
     }
 }
