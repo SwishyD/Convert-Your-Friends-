14fb1f8d4b20fc8227378244bd7c6494c4ba7c55
Melissa <mvhaydon@gmail.com>
2019-03-14 12:45:52 +1300

    New Character Prefabs and Tentacles Added

---
+++ Convert Your Friends!! Unity Project/Assets/_Scripts/Melissa/Scripts/Utility/Tentacle2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tentacle2 : MonoBehaviour
+{
+
+    Rigidbody rb;
+    float originX;
+    float originY;
+    float newX;
+    float newY;
+    public int turnSpeed;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        originX = transform.position.x;
+        originY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newAngle = transform.eulerAngles;
+        newAngle.y += turnSpeed * -1 * Time.deltaTime;
+        transform.eulerAngles = newAngle;
+    }
+}
